92eaf928d2716d480a144a0987345aff39e2c00b
Tommy Chen <tommy351@gmail.com>
2017-02-08 18:10:30 +0800

    Migrate rules from JSCS and add more rules

--- eslint.js
+++ eslint.js
@@ -38,7 +38,117 @@ module.exports = {
       args: 'none'
     }],
     'strict': [2, 'global'],
-    'require-yield': 0
+    'require-yield': 0,
+    'indent': [2, 2, {
+      SwitchCase: 1
+    }],
+    'quotes': [2, 'single'],
+    'jsx-quotes': [2, 'prefer-single'],
+    'semi': [2, 'always'],
+    'no-empty': [2, {
+      allowEmptyCatch: true
+    }],
+    'brace-style': [2, '1tbs', {
+      allowSingleLine: true
+    }],
+    'no-mixed-spaces-and-tabs': 2,
+    'no-multiple-empty-lines': 2,
+    'one-var': [2, {
+      uninitialized: 'always',
+      initialized: 'never'
+    }],
+    'dot-location': [2, 'property'],
+    'operator-linebreak': [2, 'before'],
+    'key-spacing': [2, {
+      beforeColon: false,
+      afterColon: true
+    }],
+    'space-unary-ops': [2, {
+      words: false,
+      nonwords: false
+    }],
+    'comma-spacing': [2, {
+      before: false,
+      after: true
+    }],
+    'semi-spacing': [2, {
+      before: false,
+      after: true
+    }],
+    'func-call-spacing': [2, 'never'],
+    'space-before-function-paren': [2, 'never'],
+    'space-in-parens': [2, 'never'],
+    'comma-dangle': [2, 'never'],
+    'no-trailing-spaces': 2,
+    'yoda': [2, 'never'],
+    'camelcase': 2,
+    'comma-style': [2, 'last'],
+    'curly': [2, 'multi-line', 'consistent'],
+    'func-style': [2, 'declaration', {
+      allowArrowFunctions: true
+    }],
+    'eol-last': 2,
+    'wrap-iife': 2,
+    'space-infix-ops': 2,
+    'keyword-spacing': [2, {
+      before: true,
+      after: true
+    }],
+    'space-before-blocks': [2, 'always'],
+    'no-extra-parens': [2, 'all', {
+      ignoreJSX: 'all',
+      conditionalAssign: false,
+      returnAssign: false,
+      nestedBinaryExpressions: false
+    }],
+    'no-unsafe-negation': 2,
+    'array-callback-return': 2,
+    'no-empty-pattern': 2,
+    'no-eval': 2,
+    'no-extra-label': 2,
+    'no-implicit-globals': 2,
+    'no-new-func': 2,
+    'no-new': 2,
+    'no-proto': 2,
+    'no-sequences': 2,
+    'no-useless-escape': 2,
+    'no-useless-return': 2,
+    'no-void': 2,
+    'prefer-promise-reject-errors': 2,
+    'no-label-var': 2,
+    'no-undef-init': 2,
+    'array-bracket-spacing': [2, 'never'],
+    'computed-property-spacing': [2, 'never'],
+    'block-spacing': [2, 'always'],
+    'consistent-this': 2,
+    'func-name-matching': [2, 'always'],
+    'lines-around-comment': [2, {
+      beforeBlockComment: true
+    }],
+    'new-cap': 2,
+    'new-parens': 2,
+    'no-array-constructor': 2,
+    'no-nested-ternary': 2,
+    'no-new-object': 2,
+    'no-unneeded-ternary': 2,
+    'no-whitespace-before-property': 2,
+    'spaced-comment': [2, 'always'],
+    'template-tag-spacing': [2, 'never'],
+    'unicode-bom': [2, 'never'],
+    'arrow-spacing': 2,
+    'generator-star-spacing': [2, 'after'],
+    'no-confusing-arrow': [2, {
+      allowParens: true
+    }],
+    'no-duplicate-imports': 2,
+    'no-useless-computed-key': 2,
+    'no-useless-constructor': 2,
+    'no-useless-rename': 2,
+    'prefer-const': 1,
+    'rest-spread-spacing': [2, 'never'],
+    'template-curly-spacing': [2, 'never'],
+    'yield-star-spacing': [2, 'after'],
+    'no-mixed-operators': 2
   },
   env: {
     node: true,
